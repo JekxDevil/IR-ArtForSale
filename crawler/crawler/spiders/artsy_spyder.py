7a7eb761babfe3ae57cdd0df5149daef1d527e07
Filippo Piloni <filippo.piloni@usi.ch>
2023-11-11 10:51:34 +0100

    Added report

--- crawler/crawler/spiders/artsy_spyder.py
+++ crawler/crawler/spiders/artsy_spyder.py
@@ -43,36 +43,32 @@ class ArtsySpider(scrapy.Spider):
             "//div[@class='Box-sc-15se88d-0 Flex-cw39ct-0 ArtworkGrid__InnerContainer-uextjn-1 LbPWX jdYVcL fresnel-greaterThanOrEqual-lg']")
         tag = response.xpath("//h1[@class='Box-sc-15se88d-0 Text-sc-18gcpao-0 eGPiVT bTXFzS']/text()").get()
         for elem in columns:
-            img = elem.xpath('//div[@data-test="artworkGridItem"]//img/@src').get()
+            url = elem.xpath("//a[@class='RouterLink__RouterAwareLink-sc-1nwbtp5-0 hbVDGn GridItem__Link-d2n1vy-0 JunK']/@href").get()
 
-            author = elem.xpath(
-                ".//div[@class='Box-sc-15se88d-0 Text-sc-18gcpao-0  ilQWRL']//span/text()").get()
-            name = elem.xpath(
-                ".//div[@class='Box-sc-15se88d-0 Text-sc-18gcpao-0 caIGcn iVSzqj']//i/text()").get()
-            price = elem.xpath(
-                ".//div[@class='Box-sc-15se88d-0 Text-sc-18gcpao-0 eXbAnU bfCidL']/text()").get()
-            date = elem.xpath(".//div[@class='Box-sc-15se88d-0 Text-sc-18gcpao-0 caIGcn iVSzqj']/text()").get()
-
-            if img:
-                img = img.strip()
-            if author:
-                author = author.strip()
-            if name:
-                name = name.strip()
-            if date:
-                date = date.strip()
-            if price:
-                price = price.strip()
                 
             # href_item_page = elem.xpath('')
-            if  author or name or date or price:
-                yield {'img': img, 'author': author, 'name': name, 'date':date, 'price': price, 'category': tag,
-                       }
+            if url:
+            # Ensure that the URL is absolute
+                url = response.urljoin(url)
+
+                yield scrapy.Request(url, callback=self.parseArt)
 
         next_page = response.xpath('//a[@data-testid="next"]/@href').get()
         if next_page:
             yield response.follow(next_page, callback=self.parse)
 
+
+    def parseArt(self, response):
+        price = response.xpath("//div[@class='Box-sc-15se88d-0 Text-sc-18gcpao-0 eXbAnU drBoOI']/text()").get()
+        author = response.xpath("//a[@class='RouterLink__RouterAwareLink-sc-1nwbtp5-0 dikvRF ArtworkSidebarArtists__StyledArtistLink-eqhzb8-0 jdgrPD']/text()").get()
+        title = response.xpath("//h1[@class='Box-sc-15se88d-0 Text-sc-18gcpao-0 caIGcn bhlKfb']//i/text()").get()
+        img = response.xpath('//img[@data-testid="artwork-lightbox-image"]/@src').get()
+
+        yield {'img': img, 
+               'author': author,
+               'title': title, 
+               'price': price ,
+               'url' : response.url}
 
     @staticmethod
     def get_description():
