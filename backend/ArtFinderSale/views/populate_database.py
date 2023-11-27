88f69fa5aea8f19b4396e99bb058442320034b6c
Filippo Piloni <filippo.piloni@usi.ch>
2023-11-27 12:23:48 +0100

    Started database population

---
+++ backend/ArtFinderSale/views/populate_database.py
@@ -0,0 +1,29 @@
+import json
+from ..models import Document
+import pandas as pd
+
+
+def populate_database():
+    with open('../../crawler/saatchiart.json') as f:
+        data = json.load(f)
+        df = pd.DataFrame(data)
+        df['docno'] = [f'd{i + 1}' for i in range(len(df))]  # add docno column to doc entries
+        df.info()
+
+    # Loop through DataFrame rows and create Document objects
+    for index, row in df.iterrows():
+        new_doc = Document(
+            title=row['title'],
+            author=row['author'],
+            description=row['description'],
+            url=row['url'],
+            image=row['img'],
+            tags=row['tags'],
+            price=row['price'],
+            docno=row['docno'],
+        )
+        new_doc.save()
+
+
+if __name__ == "__main__":
+    populate_database()
